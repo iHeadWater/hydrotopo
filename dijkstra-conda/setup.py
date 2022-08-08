18783fb2f33ff5a4ba2319dc23d5c4dbd5af6199
forestbat <k678900000@163.com>
2022-08-08 18:20:54 +0800

    Initial Commit

---
+++ dijkstra-conda/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import random
+import string
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='dijkstra-conda',
+    version=''.join(random.sample(string.digits, 8)),
+    description='A test project',
+    long_description='https://readthedocs.org/projects/calctest/',
+    author='dijkstra-conda-test',
+    url='https://github.com/iHeadWater/dijkstra-conda',
+    packages=[
+        'dijkstra-conda',
+    ],
+    package_dir={'dijkstra-conda': 'dijkstra-conda'},
+    include_package_data=True,
+    install_requires=[
+    ],
+    license='MIT',
+    zip_safe=False,
+    keywords='dijkstra-conda',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)
---
+++ dijkstra-conda/test/test_dijkstra-conda.py
@@ -0,0 +1,19 @@
+"""
+Tests for `dijkstra-conda` module.
+"""
+import pytest
+from dijkstra-conda import dijkstra-conda
+
+
+class TestDijkstra-conda(object):
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    def test_something(self):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
